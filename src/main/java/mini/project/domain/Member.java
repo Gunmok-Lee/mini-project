be64995e27caece6e665aed110ddda818f00dbb6
antoniolee <antoniolee@localhost>
2020-09-09 14:47:36 +0900

    Member 클래스 구현

--- src/main/java/mini/project/domain/Member.java
+++ src/main/java/mini/project/domain/Member.java
@@ -1,5 +1,64 @@
 package mini.project.domain;
 
+import java.sql.Date;
+
 public class Member {
+  private int no; // 번호
+  private String name; // 이름
+  private int age; // 나이
+  private String gender; // 성별
+  private String email; // 이메일
+  private String password; // 패스워드
+  private String tel; // 전화번호
+  private Date registeredDate; // 가입일
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public String getGender() {
+    return gender;
+  }
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getTel() {
+    return tel;
+  }
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+  public Date getRegisteredDate() {
+    return registeredDate;
+  }
+  public void setRegisteredDate(Date registeredDate) {
+    this.registeredDate = registeredDate;
+  }
+
 
 }
