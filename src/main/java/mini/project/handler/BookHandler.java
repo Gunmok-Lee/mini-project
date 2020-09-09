7f45014c1c4c6464d03f69394b68ec48cd09120b
LeeYongMin01 <vbn0213@gmail.com>
2020-09-09 18:40:28 +0900

    BookHandler 기능구현

--- src/main/java/mini/project/handler/BookHandler.java
+++ src/main/java/mini/project/handler/BookHandler.java
@@ -21,10 +21,10 @@ public class BookHandler {
     
     Book book = new Book();
     book.setBookNo(Prompt.inputInt("도서 번호: "));
-    book.setBookName(Prompt.inputString("도서 이름: "));
+    book.setBookName(Prompt.inputString("도서명: "));
     book.setCompany(Prompt.inputString("출판사: "));
     book.setAuthor(Prompt.inputString("저자: "));
-    book.setRegisteredDate(Prompt.inputDate("대여 일시: "));
+    book.setRegisteredDate(Prompt.inputDate("대여 일자: "));
     
     while (true) {
       String id = Prompt.inputString("아이디: ");
@@ -33,14 +33,14 @@ public class BookHandler {
         System.out.println("도서 대여를 취소합니다.");
         return;
       } else if (memberHandler.findByName(id) != null) { 
-        member.setName(id); // 등록된 아이디와 일치할 경우
+        member.setId(id); // 등록된 아이디와 일치할 경우
+        System.out.printf("%s님, 대여가 완료되었습니다", id);
         break;
       }
       System.out.println("아이디가 일치하지 않습니다."); // 등록된 회원과 이름이 일치하지 않을 경우
     }
     
     bookList.add(book);
-    System.out.println("대여가 완료되었습니다.");
   }
  
   public void list() {
@@ -50,12 +50,14 @@ public class BookHandler {
 
     while (iterator.hasNext()) {
       Book book = iterator.next();
-      System.out.printf("%d, %s, %s, %s, %s\n",
+      System.out.printf("도서 번호: %d\n,"
+          + "도서명: %s\n,출판사: %s\n,저자: %s\n,대여 일자: %s\n,대여자: %s\n",
           book.getBookNo(),
           book.getBookName(),
           book.getCompany(),
           book.getAuthor(),
           book.getRegisteredDate());
+          member.getName();
     }
   }
 }
