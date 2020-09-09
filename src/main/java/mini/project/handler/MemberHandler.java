e937fb14fd3c8795a6291c09db62f33a8e6dc137
antoniolee <antoniolee@localhost>
2020-09-09 15:20:51 +0900

    MemberHandler 구현

--- src/main/java/mini/project/handler/MemberHandler.java
+++ src/main/java/mini/project/handler/MemberHandler.java
@@ -1,5 +1,28 @@
 package mini.project.handler;
 
-public class MemberHandler {
+import java.util.List;
+import com.eomcs.util.Prompt;
+import mini.project.domain.Member;
 
+public class MemberHandler {
+  List<Member> memberList;
+
+  public MemberHandler(List<Member> list) {
+    this.memberList = list;
+  }
+
+  public void add() {
+    System.out.println("[회원 가입]");
+
+    Member member = new Member();
+    member.setNo(Prompt.inputInt("번호? "));
+    member.setName(Prompt.inputString("이름? "));
+    member.setEmail(Prompt.inputString("이메일? "));
+    member.setPassword(Prompt.inputString("암호? "));
+    member.setPhoto(Prompt.inputString("사진? "));
+    member.setTel(Prompt.inputString("전화? "));
+    member.setRegisteredDate(new java.sql.Date(System.currentTimeMillis()));
+
+    memberList.add(member);
+  }
 }
