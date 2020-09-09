b0ec07c05e6a53466e78fb7faf980840ae6ad0ce
LeeYongMin01 <vbn0213@gmail.com>
2020-09-09 15:16:00 +0900

    프롬프프

---
+++ src/main/java/mini/project/util/Prompt.java
@@ -0,0 +1,25 @@
+package mini.project.util;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class Prompt {
+  static Scanner scanner = new Scanner(System.in);
+  
+  public static String inputString(String title) {
+    System.out.println(title);
+    return scanner.nextLine();
+  }
+  
+  public static int inputInt(String title) {
+    return Integer.parseInt(inputString(title));
+  }
+  
+  public static Date inputDate(String title) {
+    return Date.valueOf(inputString(title));
+  }
+  
+  public static void close() {
+    scanner.close();
+  }
+}
