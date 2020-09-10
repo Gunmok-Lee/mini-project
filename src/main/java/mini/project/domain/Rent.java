62f38e34854c81ec86219c126d2dfed2ab5fd58a
LeeYongMin01 <vbn0213@gmail.com>
2020-09-10 09:59:19 +0900

    update

--- src/main/java/mini/project/domain/Rent.java
+++ src/main/java/mini/project/domain/Rent.java
@@ -1,6 +1,16 @@
 package mini.project.domain;
 
+import java.sql.Date;
+
 public class Rent {
 
+  private Date registeredDate; // 대여 일자
+  
+  public Date getRegisteredDate() {
+    return registeredDate;
+  }
+  public void setRegisteredDate(Date registeredDate) {
+    this.registeredDate = registeredDate;
+  }
 }
  
--- src/main/java/mini/project/handler/RentHandler.java
+++ src/main/java/mini/project/handler/RentHandler.java
@@ -1,5 +1,16 @@
 package mini.project.handler;
 
+import java.util.List;
+import mini.project.domain.Book;
 public class RentHandler {
  
+  List RentList;
+  Book book;
+  
+  public void rent() {
+    System.out.println("[도서 대여]");
+    
+    Object object = new Object();
+    
+  }
 }
