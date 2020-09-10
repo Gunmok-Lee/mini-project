b2e4dab7771f79fb66f4a0f325a78542b62cc573
antoniolee <antoniolee@localhost>
2020-09-10 10:06:10 +0900

    rent수정

--- src/main/java/mini/project/domain/Rent.java
+++ src/main/java/mini/project/domain/Rent.java
@@ -3,14 +3,28 @@ package mini.project.domain;
 import java.sql.Date;
 
 public class Rent {
+  private String rentowner;//대여자
+  private String rentAble;//대여가능여부
+  private Date rentDate;//대여일
 
-  private Date registeredDate; // 대여 일자
-  
-  public Date getRegisteredDate() {
-    return registeredDate;
+  public String getRentowner() {
+    return rentowner;
   }
-  public void setRegisteredDate(Date registeredDate) {
-    this.registeredDate = registeredDate;
+  public void setRentowner(String rentowner) {
+    this.rentowner = rentowner;
   }
+  public String getRentAble() {
+    return rentAble;
+  }
+  public void setRentAble(String rentAble) {
+    this.rentAble = rentAble;
+  }
+  public Date getRentDate() {
+    return rentDate;
+  }
+  public void setRentDate(Date rentDate) {
+    this.rentDate = rentDate;
+  }
+
+
 }
- 
